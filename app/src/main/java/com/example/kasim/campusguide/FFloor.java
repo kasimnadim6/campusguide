2f71a22d7a4b662e14fc99ae52bd9fdf1474c7b4
iamkasim <kasimnadim6@gmail.com>
2018-02-25 07:46:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kasim/campusguide/FFloor.java
@@ -0,0 +1,13 @@
+package com.example.kasim.campusguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FFloor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ffloor);
+    }
+}
---
+++ app/src/main/java/com/example/kasim/campusguide/splashscreen.java
@@ -0,0 +1,44 @@
+package com.example.kasim.campusguide;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
+
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        SharedPreferences settings = getSharedPreferences("prefs",0);
+        boolean FirstRun = settings.getBoolean("FirstRun",true);
+
+        if(FirstRun) {
+            //do something
+            new Thread() {
+                public void run() {
+                    try {
+                        sleep(1000);
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }else {
+
+            Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(myIntent);
+            finish();
+        }
+    }
+}
